9de3d8e1bf7c11609a86b2bd40afb25dd27e7e4f
Chris Salzberg <chris@dejimata.com>
2016-11-24 15:01:41 +0900

    Extend model with Translates

--- lib/mobility.rb
+++ lib/mobility.rb
@@ -6,8 +6,20 @@ module Mobility
   autoload :Attributes,    "mobility/attributes"
   autoload :Backend,       "mobility/backend"
   autoload :Configuration, "mobility/configuration"
+  autoload :Translates,    "mobility/translates"
 
   class << self
+    def extended(model_class)
+      return if model_class.respond_to? :translation_accessor
+      model_class.class_eval do
+        extend Translates
+      end
+    end
+
+    def included(model_class)
+      model_class.extend self
+    end
+
     def locale
       read_locale || I18n.locale
     end
--- spec/mobility_spec.rb
+++ spec/mobility_spec.rb
@@ -5,6 +5,37 @@ describe Mobility do
     expect(Mobility::VERSION).not_to be nil
   end
 
+  describe "including Mobility in class" do
+    let!(:model) do
+      model = stub_const 'MyModel', Class.new
+      model.class_eval do
+        def attributes
+          { "foo" => "bar" }
+        end
+      end
+      model
+    end
+
+    context "with no translated attributes" do
+      it "does not include Attributes into model class" do
+        expect(Mobility::Attributes).not_to receive(:new)
+        model.include Mobility
+      end
+    end
+
+    context "with translated attributes" do
+      it "includes backend module into model class" do
+        expect(Mobility::Attributes).to receive(:new).and_call_original
+        model.include Mobility
+        model.translates :title, backend: :null, foo: :bar
+        backend_module = model.ancestors.find { |a| a.class == Mobility::Attributes }
+        expect(backend_module).not_to be_nil
+        expect(backend_module.attributes).to eq ["title"]
+        expect(backend_module.options).to eq("foo" => :bar)
+      end
+    end
+  end
+
   describe '.with_locale' do
     def perform_with_locale(locale)
       Thread.new do

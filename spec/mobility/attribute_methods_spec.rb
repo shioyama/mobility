f55aa8667f3bf5a78ddf480f5c2ad76036f3b54d
Chris Salzberg <chris@dejimata.com>
2016-11-24 23:06:09 +0900

    Add spec for AttributeMethods

---
+++ spec/mobility/attribute_methods_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe Mobility::AttributeMethods do
+  before do
+    model = stub_const 'BaseModel', Class.new
+    model.class_eval do
+      def attributes; { "untranslated" => "bar" }; end
+    end
+    mobility_model = stub_const 'MobilityModel', Class.new(BaseModel)
+    klass = described_class
+    mobility_model.class_eval do
+      def self.translated_attribute_names; ["title"]; end
+      def title; "foo"; end
+      include klass
+    end
+  end
+
+  subject { MobilityModel.new }
+
+  describe "#translated_attribute_names" do
+    its(:translated_attribute_names) { should == ["title"] }
+  end
+
+  describe "#translated_attributes" do
+    it "returns hash of translated attribute names/values" do
+      expect(subject.translated_attributes).to eq(
+        {
+          "title" => "foo"
+        }
+      )
+    end
+  end
+
+  describe "#attributes" do
+    it "adds translated attributes to normal attributes" do
+      expect(subject.attributes).to eq(
+        {
+          "untranslated" => "bar",
+          "title" => "foo"
+        }
+      )
+    end
+  end
+end

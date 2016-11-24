c72bb5b16dd70278215aa65fbaad6e20815bdbd5
Chris Salzberg <chris@dejimata.com>
2016-11-24 14:29:31 +0900

    Add locale methods

--- lib/mobility.rb
+++ lib/mobility.rb
@@ -6,6 +6,24 @@ module Mobility
   autoload :Configuration, "mobility/configuration"
 
   class << self
+    def locale
+      read_locale || I18n.locale
+    end
+
+    def locale=(locale)
+      set_locale(locale)
+    end
+
+    def with_locale(locale)
+      previous_locale = read_locale
+      begin
+        set_locale(locale)
+        yield(locale)
+      ensure
+        set_locale(previous_locale)
+      end
+    end
+
     def storage
       RequestStore.store
     end
@@ -14,5 +32,17 @@ module Mobility
       storage[:mobility_configuration] ||= Mobility::Configuration.new
     end
     delegate :default_fallbacks, to: :config
+
+    protected
+
+    def read_locale
+      storage[:mobility_locale]
+    end
+
+    def set_locale(locale)
+      locale = locale.try(:to_sym)
+      raise Mobility::InvalidLocale.new(locale) unless I18n.available_locales.include?(locale) || locale.nil?
+      storage[:mobility_locale] = locale.try(:to_sym)
+    end
   end
 end
--- spec/mobility_spec.rb
+++ spec/mobility_spec.rb
@@ -5,6 +5,43 @@ describe Mobility do
     expect(Mobility::VERSION).not_to be nil
   end
 
+  describe '.with_locale' do
+    def perform_with_locale(locale)
+      Thread.new do
+        Mobility.with_locale(locale) do
+          Thread.pass
+          expect(locale).to eq(Mobility.locale)
+        end
+      end
+    end
+
+    it 'sets locale in a single thread' do
+      perform_with_locale(:en).join
+    end
+
+    it 'sets independent locales in multiple threads' do
+      threads = []
+      threads << perform_with_locale(:en)
+      threads << perform_with_locale(:fr)
+      threads << perform_with_locale(:de)
+      threads << perform_with_locale(:cz)
+      threads << perform_with_locale(:pl)
+
+      threads.each(&:join)
+    end
+
+    it "returns result" do
+      expect(Mobility.with_locale(:ja) { |locale| "returned-#{locale}" }).to eq("returned-ja")
+    end
+
+    context "something blows up" do
+      it "sets locale back" do
+        Mobility.with_locale(:ja) { raise StandardError } rescue StandardError
+        expect(Mobility.locale).to eq(:en)
+      end
+    end
+  end
+
   describe '.config' do
     it 'initializes a new configuration' do
       expect(Mobility.config).to be_a(Mobility::Configuration)

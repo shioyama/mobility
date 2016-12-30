7ddca9c1a43374da04ce2acc0efb594cc0155adf
Chris Salzberg <chris@dejimata.com>
2016-12-30 14:43:14 +0900

    Use type option in spec models

--- spec/active_record/models.rb
+++ spec/active_record/models.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
   extend Mobility
-  translates :title, backend: :table, cache: true, locale_accessors: true, dirty: true
-  translates :content, backend: :table, cache: true, locale_accessors: true, dirty: true, table_class: Mobility::ActiveRecord::StringTranslation
-  translates :subtitle, backend: :table, cache: true, locale_accessors: true, dirty: true
+  translates :title, backend: :table, cache: true, locale_accessors: true, dirty: true, type: :string
+  translates :content, backend: :table, cache: true, locale_accessors: true, dirty: true, type: :text
 end
 
 class FallbackPost < ActiveRecord::Base
--- spec/sequel/models.rb
+++ spec/sequel/models.rb
@@ -3,7 +3,8 @@ Sequel::Model.db = DB
 class Post < Sequel::Model
   plugin :dirty
   extend Mobility
-  translates :title, :content, backend: :table, cache: true, locale_accessors: true, dirty: true
+  translates :title, backend: :table, cache: true, locale_accessors: true, dirty: true, type: :string
+  translates :content, backend: :table, cache: true, locale_accessors: true, dirty: true, type: :text
 end
 
 class FallbackPost < Sequel::Model

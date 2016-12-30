e039a94daefa021282d06a46b7571a827bd34720
Chris Salzberg <chris@dejimata.com>
2016-12-30 11:27:32 +0900

    Add mobility scope to Sequel models

--- lib/mobility/sequel.rb
+++ lib/mobility/sequel.rb
@@ -2,5 +2,15 @@ module Mobility
   module Sequel
     autoload :Translation,     "mobility/sequel/translation"
     autoload :BackendResetter, "mobility/sequel/backend_resetter"
+
+    def self.included(model_class)
+      model_class.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def i18n
+        dataset
+      end
+    end
   end
 end
---
+++ spec/mobility/sequel_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "Mobility::Sequel", orm: :sequel do
+  before do
+    stub_const 'MyModel', Sequel::Model
+    MyModel.include Mobility::Sequel
+  end
+
+  describe ".i18n" do
+    it "extends class with .i18n dataset method" do
+      dataset = double('dataset')
+      expect(MyModel).to receive(:dataset).and_return(dataset)
+
+      expect(MyModel.i18n).to eq(dataset)
+    end
+  end
+end

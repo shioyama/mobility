ab178e0040dc4d6f9757730b2a967ef5b995f299
Chris Salzberg <chris@dejimata.com>
2016-12-30 11:33:00 +0900

    Add Mobility::Backend::ActiveRecord::QueryMethods module

--- lib/mobility/backend/active_record.rb
+++ lib/mobility/backend/active_record.rb
@@ -1,8 +1,9 @@
 module Mobility
   module Backend
     module ActiveRecord
-      autoload :Columns, 'mobility/backend/active_record/columns'
-      autoload :Table,   'mobility/backend/active_record/table'
+      autoload :Columns,      'mobility/backend/active_record/columns'
+      autoload :Table,        'mobility/backend/active_record/table'
+      autoload :QueryMethods, 'mobility/backend/active_record/query_methods'
     end
   end
 end
---
+++ lib/mobility/backend/active_record/query_methods.rb
@@ -0,0 +1,8 @@
+module Mobility
+  module Backend
+    module ActiveRecord
+      class QueryMethods < Module
+      end
+    end
+  end
+end

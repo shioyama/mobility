e7a6d72d06347be97fd3795f294459293533c712
Chris Salzberg <chris@dejimata.com>
2017-01-15 14:32:33 +0900

    Fall through to default backend to attributes.backend_name if present

--- lib/mobility/attributes.rb
+++ lib/mobility/attributes.rb
@@ -7,7 +7,7 @@ module Mobility
       @options = _options
       @attributes = _attributes.map &:to_s
       model_class = options.delete(:model_class)
-      @backend_name = options.delete(:backend)
+      @backend_name = options.delete(:backend) || Mobility.config.default_backend
       @backend_class = Class.new(get_backend_class(backend:     @backend_name,
                                                    model_class: model_class))
 
@@ -68,7 +68,6 @@ module Mobility
     end
 
     def get_backend_class(backend: nil, model_class: nil)
-      backend ||= Mobility.config.default_backend
       raise Mobility::BackendRequired, "Backend option required if Mobility.config.default_backend is not set." if backend.nil?
       klass = Module === backend ? backend : Mobility::Backend.const_get(backend.to_s.camelize.gsub(/\s+/, ''))
       model_class.nil? ? klass : klass.for(model_class)

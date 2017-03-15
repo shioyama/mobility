4a6bddf76d7cc2f5eef443787cba92d8b06f574b
Chris Salzberg <chris@dejimata.com>
2017-03-15 12:08:09 +0900

    Refactor Sequel QueryMethods

--- lib/mobility/backend/sequel/key_value/query_methods.rb
+++ lib/mobility/backend/sequel/key_value/query_methods.rb
@@ -3,9 +3,21 @@ module Mobility
     class Sequel::KeyValue::QueryMethods < Sequel::QueryMethods
       def initialize(attributes, **options)
         super
-        attributes_extractor = @attributes_extractor
         association_name, translations_class = options[:association_name], options[:class_name]
 
+        define_join_method(association_name, translations_class)
+        define_query_methods(association_name)
+
+        attributes.each do |attribute|
+          define_method :"first_by_#{attribute}" do |value|
+            where(attribute => value).select_all(model.table_name).first
+          end
+        end
+      end
+
+      private
+
+      def define_join_method(association_name, translations_class)
         define_method :"join_#{association_name}" do |*attributes, **options|
           attributes.inject(self) do |relation, attribute|
             join_type = options[:outer_join] ? :left_outer : :inner
@@ -20,9 +32,14 @@ module Mobility
                                 table_alias: "#{attribute}_#{association_name}")
           end
         end
+      end
 
+      def define_query_methods(association_name)
         # TODO: find a better way to do this that doesn't involve overriding
         # a private method...
+        #
+        attributes_extractor = @attributes_extractor
+
         define_method :_filter_or_exclude do |invert, clause, *cond, &block|
           if i18n_keys = attributes_extractor.call(cond.first)
             cond = cond.first.dup
@@ -37,11 +54,6 @@ module Mobility
         end
         private :_filter_or_exclude
 
-        attributes.each do |attribute|
-          define_method :"first_by_#{attribute}" do |value|
-            where(attribute => value).select_all(model.table_name).first
-          end
-        end
       end
     end
   end

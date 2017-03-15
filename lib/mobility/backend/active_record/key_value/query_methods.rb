d3b5b072363d069be80f3bfc86415e76950c734e
Chris Salzberg <chris@dejimata.com>
2017-03-15 12:18:46 +0900

    Refactor AR QueryMethods

--- lib/mobility/backend/active_record/key_value/query_methods.rb
+++ lib/mobility/backend/active_record/key_value/query_methods.rb
@@ -5,33 +5,9 @@ module Mobility
         super
         association_name, translations_class = options[:association_name], options[:class_name]
         @association_name    = association_name
-        attributes_extractor = @attributes_extractor
 
-        define_method :"join_#{association_name}" do |*attributes, **options|
-          attributes.inject(self) do |relation, attribute|
-            t = translations_class.arel_table.alias(:"#{attribute}_#{association_name}")
-            m = arel_table
-            join_type = options[:outer_join] ? Arel::Nodes::OuterJoin : Arel::Nodes::InnerJoin
-            relation.joins(m.join(t, join_type).
-                           on(t[:key].eq(attribute).
-                              and(t[:locale].eq(Mobility.locale).
-                                  and(t[:translatable_type].eq(name).
-                                      and(t[:translatable_id].eq(m[:id]))))).join_sources)
-          end
-        end
-
-        define_method :where! do |opts, *rest|
-          if i18n_keys = attributes_extractor.call(opts)
-            opts = opts.with_indifferent_access
-            i18n_nulls = i18n_keys.select { |key| opts[key].nil? }
-            i18n_keys.each { |attr| opts["#{attr}_#{association_name}"] = { value: opts.delete(attr) }}
-            super(opts, *rest).
-              send("join_#{association_name}", *(i18n_keys - i18n_nulls)).
-              send("join_#{association_name}", *i18n_nulls, outer_join: true)
-          else
-            super(opts, *rest)
-          end
-        end
+        define_join_method(association_name, translations_class)
+        define_query_methods(association_name)
 
         attributes.each do |attribute|
           define_method :"find_by_#{attribute}" do |value|
@@ -57,6 +33,40 @@ module Mobility
           end
         end
         relation.model.mobility_where_chain.prepend(mod)
+      end
+
+      private
+
+      def define_join_method(association_name, translations_class)
+        define_method :"join_#{association_name}" do |*attributes, **options|
+          attributes.inject(self) do |relation, attribute|
+            t = translations_class.arel_table.alias(:"#{attribute}_#{association_name}")
+            m = arel_table
+            join_type = options[:outer_join] ? Arel::Nodes::OuterJoin : Arel::Nodes::InnerJoin
+            relation.joins(m.join(t, join_type).
+                           on(t[:key].eq(attribute).
+                              and(t[:locale].eq(Mobility.locale).
+                                  and(t[:translatable_type].eq(name).
+                                      and(t[:translatable_id].eq(m[:id]))))).join_sources)
+          end
+        end
+      end
+
+      def define_query_methods(association_name)
+        attributes_extractor = @attributes_extractor
+
+        define_method :where! do |opts, *rest|
+          if i18n_keys = attributes_extractor.call(opts)
+            opts = opts.with_indifferent_access
+            i18n_nulls = i18n_keys.select { |key| opts[key].nil? }
+            i18n_keys.each { |attr| opts["#{attr}_#{association_name}"] = { value: opts.delete(attr) }}
+            super(opts, *rest).
+              send("join_#{association_name}", *(i18n_keys - i18n_nulls)).
+              send("join_#{association_name}", *i18n_nulls, outer_join: true)
+          else
+            super(opts, *rest)
+          end
+        end
       end
     end
   end

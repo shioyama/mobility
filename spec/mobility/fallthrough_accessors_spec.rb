9bc82c62513b954fc00ec7c7af89589d78b2ad81
Chris Salzberg <chris@dejimata.com>
2017-04-04 19:10:29 +0900

    Refactor FallthroughAccessors class spec

--- spec/mobility/fallthrough_accessors_spec.rb
+++ spec/mobility/fallthrough_accessors_spec.rb
@@ -2,51 +2,20 @@ require "spec_helper"
 
 describe Mobility::FallthroughAccessors do
   let(:model_class) do
-    model_class = stub_const 'MyModel', Class.new
-    model_class.class_eval do
-      def title
-        values[Mobility.locale]
-      end
-
-      def title?
-        values[Mobility.locale].present?
-      end
-
-      def title=(value)
-        values[Mobility.locale] = value
-      end
-
-      private
-
-      def values
-        @values ||= {}
-      end
+    klass = Class.new do
+      def title(**_); end
+      def title?(**_); end
+      def title=(_value, **_); end
     end
-    model_class.include(described_class.new(:title))
-    model_class
+    klass.include described_class.new(:title)
+    klass
   end
+  let(:options) { { these: "options" } }
 
-  it "handles getters and setters in any locale" do
-    instance = model_class.new
-    expect(instance.title_fr).to eq(nil)
-    expect(instance.title_de).to eq(nil)
-    instance.title_fr = "Titre"
-    expect(instance.title_fr).to eq("Titre")
-    instance.title_de = "Titel"
-    expect(instance.title_de).to eq("Titel")
-  end
-
-  it "handles presence methods in any locale" do
-    instance = model_class.new
-    expect(instance.title_fr?).to eq(false)
-    instance.title_fr = ""
-    expect(instance.title_fr?).to eq(false)
-    instance.title_fr = "Titre"
-    expect(instance.title_fr?).to eq(true)
-  end
+  it_behaves_like "locale accessor", :en
+  it_behaves_like "locale accessor", :de
 
   it "raises InvalidLocale if locale is not in I18n.available_locales" do
-    instance = model_class.new
-    expect { instance.title_ru }.to raise_error(Mobility::InvalidLocale)
+    expect { model_class.new.title_ru }.to raise_error(Mobility::InvalidLocale)
   end
 end
--- spec/mobility/locale_accessors_spec.rb
+++ spec/mobility/locale_accessors_spec.rb
@@ -10,33 +10,6 @@ describe Mobility::LocaleAccessors do
   end
   let(:options) { { these: "options" } }
 
-  shared_examples_for "locale accessor" do |locale|
-    it "handles getters and setters in any locale in I18n.available_locales" do
-      instance = model_class.new
-
-      aggregate_failures "getter" do
-        expect(instance).to receive(:title).with(these: "options") do
-          expect(Mobility.locale).to eq(locale)
-        end.and_return("foo")
-        expect(instance.send(:"title_#{locale}", options)).to eq("foo")
-      end
-
-      aggregate_failures "presence" do
-        expect(instance).to receive(:title?).with(these: "options") do
-          expect(Mobility.locale).to eq(locale)
-        end.and_return(true)
-        expect(instance.send(:"title_#{locale}?", options)).to eq(true)
-      end
-
-      aggregate_failures "setter" do
-        expect(instance).to receive(:title=).with("value", these: "options") do
-          expect(Mobility.locale).to eq(locale)
-        end.and_return("value")
-        expect(instance.send(:"title_#{locale}=", "value", options)).to eq("value")
-      end
-    end
-  end
-
   context "locales unset" do
     before do
       @available_locales = I18n.available_locales

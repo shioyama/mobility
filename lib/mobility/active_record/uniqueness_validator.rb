2c35c8aab08bbbbf43be41d05cbeab01a22ed3d6
Chris Salzberg <chris@dejimata.com>
2017-05-05 21:42:47 +0900

    Refactor UniquenessValidator

--- lib/mobility/active_record/uniqueness_validator.rb
+++ lib/mobility/active_record/uniqueness_validator.rb
@@ -3,14 +3,12 @@ module Mobility
     class UniquenessValidator < ::ActiveRecord::Validations::UniquenessValidator
       def validate_each(record, attribute, value)
         klass = record.class
-        if klass.translated_attribute_names.include?(attribute.to_s) ||
-            (Array(options[:scope]).map(&:to_s) & klass.translated_attribute_names).present?
+
+        if ((Array(options[:scope]) + [attribute]).map(&:to_s) & klass.translated_attribute_names).present?
           return unless value.present?
           relation = klass.send(Mobility.query_method).where(attribute => value)
           relation = relation.where.not(klass.primary_key => record.id) if record.persisted?
-          Array(options[:scope]).each do |scope_item|
-            relation = relation.where(scope_item => record.send(scope_item))
-          end
+          relation = mobility_scope_relation(record, relation)
           relation = relation.merge(options[:conditions]) if options[:conditions]
 
           if relation.exists?
@@ -22,6 +20,15 @@ module Mobility
         else
           super
         end
+      end
+
+      private
+
+      def mobility_scope_relation(record, relation)
+        Array(options[:scope]).each do |scope_item|
+          relation = relation.where(scope_item => record.send(scope_item))
+        end
+        relation
       end
     end
   end

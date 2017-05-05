586f309f7e6b298d8e829a6d94a04477527cce84
Chris Salzberg <chris@dejimata.com>
2017-05-05 20:51:19 +0900

    Use Mobility.query_method instead of hard-coding i18n scope

--- lib/mobility/active_record/uniqueness_validator.rb
+++ lib/mobility/active_record/uniqueness_validator.rb
@@ -6,7 +6,7 @@ module Mobility
         if klass.translated_attribute_names.include?(attribute.to_s) ||
             (Array(options[:scope]).map(&:to_s) & klass.translated_attribute_names)
           return unless value.present?
-          relation = klass.i18n.where(attribute => value)
+          relation = klass.send(Mobility.query_method).where(attribute => value)
           relation = relation.where.not(klass.primary_key => record.id) if record.persisted?
           Array(options[:scope]).each do |scope_item|
             relation = relation.where(scope_item => record.send(scope_item))

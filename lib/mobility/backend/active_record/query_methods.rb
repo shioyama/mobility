d3e974855f7e772b5df43f665a2251a1982cfff0
Chris Salzberg <chris@dejimata.com>
2017-05-27 22:03:42 +0900

    Avoid using respond_to? on a relation, since it memoizes an arel object

--- lib/mobility/backend/active_record/query_methods.rb
+++ lib/mobility/backend/active_record/query_methods.rb
@@ -20,7 +20,7 @@ models. For details see backend-specific subclasses.
         # @note Only want to define this once, even if multiple QueryMethods
         #   modules are included, so define it here in extended method
         def extended(relation)
-          unless relation.respond_to?(:mobility_where_chain)
+          unless relation.methods(false).include?(:mobility_where_chain)
             relation.define_singleton_method(:mobility_where_chain) do
               @mobility_where_chain ||= Class.new(::ActiveRecord::QueryMethods::WhereChain)
             end

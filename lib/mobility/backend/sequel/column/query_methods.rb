a05ae0baadb41e71d2d9a3f6007355c2dca98ea3
Chris Salzberg <chris@dejimata.com>
2017-06-23 14:58:23 -0400

    Only dup if query has translated attribute in conditions

--- lib/mobility/backend/sequel/column/query_methods.rb
+++ lib/mobility/backend/sequel/column/query_methods.rb
@@ -7,7 +7,8 @@ module Mobility
 
         %w[exclude or where].each do |method_name|
           define_method method_name do |*conds, &block|
-            if keys = attributes_extractor.call(cond = conds.first.dup)
+            if keys = attributes_extractor.call(conds.first)
+              cond = conds.first.dup
               keys.each { |attr| cond[Column.column_name_for(attr)] = cond.delete(attr) }
               super(cond, &block)
             else

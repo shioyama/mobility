c99af68b020cede58975ef189d5185134e0c2584
Chris Salzberg <chris@dejimata.com>
2017-06-23 23:00:56 -0400

    Refactor postgres query methods into module

--- lib/mobility/backend/sequel/hstore/query_methods.rb
+++ lib/mobility/backend/sequel/hstore/query_methods.rb
@@ -1,54 +1,21 @@
+require 'mobility/backend/sequel/postgres_query_methods'
+
 Sequel.extension :pg_hstore, :pg_hstore_ops
 
 module Mobility
   module Backend
     class Sequel::Hstore::QueryMethods < Sequel::QueryMethods
+      include PostgresQueryMethods
+
       def initialize(attributes, **)
         super
 
-        define_query_methods
+        define_query_methods("hstore")
 
         attributes.each do |attribute|
           define_method :"first_by_#{attribute}" do |value|
             where(::Sequel.hstore(attribute.to_sym).contains(::Sequel.hstore({ Mobility.locale.to_s => value }))).
               select_all(model.table_name).first
-          end
-        end
-      end
-
-      private
-
-      def define_query_methods
-        attributes_extractor = @attributes_extractor
-
-        %w[exclude or where].each do |method_name|
-          invert = method_name == "exclude"
-
-          define_method method_name do |*cond, &block|
-            if i18n_keys = attributes_extractor.call(cond.first)
-              locale = Mobility.locale.to_s
-              table_name = model.table_name
-              cond = cond.first
-
-              i18n_query = i18n_keys.inject(::Sequel.expr(!invert)) do |expr, attr|
-                value = cond.delete(attr)
-                attr_hstore = ::Sequel.hstore_op(attr)
-                contains_value = attr_hstore.contains({ locale => value.to_s })
-                has_key = attr_hstore.has_key?(locale)
-                if invert
-                  expr.|(has_key & ~contains_value)
-                else
-                  expr.&(value.nil? ? ~has_key : contains_value)
-                end
-              end
-              if method_name == "or"
-                cond.empty? ? super(i18n_query, &block) : super(::Sequel.&(cond, i18n_query), &block)
-              else
-                super(cond, &block).where(i18n_query)
-              end
-            else
-              super(*cond, &block)
-            end
           end
         end
       end
--- lib/mobility/backend/sequel/jsonb/query_methods.rb
+++ lib/mobility/backend/sequel/jsonb/query_methods.rb
@@ -1,54 +1,21 @@
+require 'mobility/backend/sequel/postgres_query_methods'
+
 Sequel.extension :pg_json, :pg_json_ops
 
 module Mobility
   module Backend
     class Sequel::Jsonb::QueryMethods < Sequel::QueryMethods
+      include PostgresQueryMethods
+
       def initialize(attributes, **)
         super
 
-        define_query_methods
+        define_query_methods("pg_jsonb")
 
         attributes.each do |attribute|
           define_method :"first_by_#{attribute}" do |value|
             where(::Sequel.pg_jsonb_op(attribute).contains({ Mobility.locale => value })).
               select_all(model.table_name).first
-          end
-        end
-      end
-
-      private
-
-      def define_query_methods
-        attributes_extractor = @attributes_extractor
-
-        %w[exclude or where].each do |method_name|
-          invert = method_name == "exclude"
-
-          define_method method_name do |*conds, &block|
-            if i18n_keys = attributes_extractor.call(conds.first)
-              locale = Mobility.locale.to_s
-              table_name = model.table_name
-              cond = conds.first
-
-              i18n_query = i18n_keys.inject(::Sequel.expr(!invert)) do |expr, attr|
-                value = cond.delete(attr)
-                attr_jsonb = ::Sequel.pg_jsonb_op(attr)
-                contains_value = attr_jsonb.contains({ locale => value })
-                has_key = attr_jsonb.has_key?(locale)
-                if invert
-                  expr.|(has_key & ~contains_value)
-                else
-                  expr.&(value.nil? ? ~has_key : contains_value)
-                end
-              end
-              if method_name == "or"
-                cond.empty? ? super(i18n_query, &block) : super(::Sequel.&(cond, i18n_query), &block)
-              else
-                super(cond, &block).where(i18n_query)
-              end
-            else
-              super(*conds, &block)
-            end
           end
         end
       end

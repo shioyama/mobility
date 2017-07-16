d8fe42f81211640125e6a50bf681d45dbaa71c40
Chris Salzberg <chris@dejimata.com>
2017-07-16 14:40:20 +0900

    Override attributes method using anon module

--- lib/mobility/active_record/attribute_methods.rb
+++ lib/mobility/active_record/attribute_methods.rb
@@ -13,12 +13,14 @@ ancestors.
       # @return [Array<String>] Model attributes
       # @!method attributes
       def self.included(model)
-        model.class_eval do
-          alias_method :untranslated_attributes, :attributes
-
+        attributes_method = Module.new do
           def attributes
             super.merge(translated_attributes)
           end
+        end
+        model.class_eval do
+          alias_method :untranslated_attributes, :attributes
+          include attributes_method
         end
       end
 

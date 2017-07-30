fa830479b4bfe12eccd77cb66b138b07b881c041
Chris Salzberg <chris@dejimata.com>
2017-07-30 21:38:37 +0900

    Call string methods if defined in Mobility::Util

--- lib/mobility/util.rb
+++ lib/mobility/util.rb
@@ -1,6 +1,11 @@
 require "singleton"
 
 module Mobility
+=begin
+
+Some useful methods on strings, borrowed in parts from Sequel and ActiveSupport.
+
+=end
   module Util
     VALID_CONSTANT_NAME_REGEXP = /\A(?:::)?([A-Z]\w*(?:::[A-Z]\w*)*)\z/.freeze
 
@@ -12,7 +17,9 @@ module Mobility
     # @param [String] str
     # @return [String]
     def camelize(str)
-      str.to_s.sub(/^[a-z\d]*/) { $&.capitalize }.gsub(/(?:_|(\/))([a-z\d]*)/) { "#{$1}#{$2.capitalize}" }.gsub('/'.freeze, '::'.freeze)
+      call_or_yield str do
+        str.to_s.sub(/^[a-z\d]*/) { $&.capitalize }.gsub(/(?:_|(\/))([a-z\d]*)/) { "#{$1}#{$2.capitalize}" }.gsub('/'.freeze, '::'.freeze)
+      end
     end
 
     # Tries to find a constant with the name specified in the argument string.
@@ -20,8 +27,10 @@ module Mobility
     # @return [Object]
     def constantize(str)
       str = str.to_s
-      raise(NameError, "#{s.inspect} is not a valid constant name!") unless m = VALID_CONSTANT_NAME_REGEXP.match(str)
-      Object.module_eval("::#{m[1]}", __FILE__, __LINE__)
+      call_or_yield str do
+        raise(NameError, "#{s.inspect} is not a valid constant name!") unless m = VALID_CONSTANT_NAME_REGEXP.match(str)
+        Object.module_eval("::#{m[1]}", __FILE__, __LINE__)
+      end
     end
 
     # Returns the singular form of a word in a string.
@@ -29,29 +38,49 @@ module Mobility
     # @return [String]
     # @note Simply strips the trailing 's' from a string.
     def singularize(str)
-      str.to_s.gsub(/s$/, '')
+      call_or_yield str do
+        str.to_s.gsub(/s$/, '')
+      end
     end
 
     # Removes the module part from the expression in the string.
     # @param [String] str
     # @return [String]
     def demodulize(str)
-      str.to_s.gsub(/^.*::/, '')
+      call_or_yield str do
+        str.to_s.gsub(/^.*::/, '')
+      end
     end
 
     # Creates a foreign key name from a class name
     # @param [String] str
     # @return [String]
     def foreign_key(str)
-      "#{underscore(demodulize(str))}_id"
+      call_or_yield str do
+        "#{underscore(demodulize(str))}_id"
+      end
     end
 
     # Makes an underscored, lowercase form from the expression in the string.
     # @param [String] str
     # @return [String]
     def underscore(str)
-      str.to_s.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-        gsub(/([a-z\d])([A-Z])/,'\1_\2').tr("-", "_").downcase
+      call_or_yield str do
+        str.to_s.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+          gsub(/([a-z\d])([A-Z])/,'\1_\2').tr("-", "_").downcase
+      end
+    end
+
+    private
+
+    # Calls caller method on object if defined, otherwise yields to block
+    def call_or_yield(object)
+      caller_method = caller_locations(1,1)[0].label
+      if object.respond_to?(caller_method)
+        object.public_send(caller_method)
+      else
+        yield
+      end
     end
 
     extend self

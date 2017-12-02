d69e5c137844f81e870ec585f95adc19cdb46ed9
Chris Salzberg <chris@dejimata.com>
2017-12-02 22:37:04 +0900

    Use module_eval to define query scope, and name module

--- lib/mobility/active_record.rb
+++ lib/mobility/active_record.rb
@@ -7,13 +7,18 @@ Module loading ActiveRecord-specific classes for Mobility models.
   module ActiveRecord
     require "mobility/active_record/uniqueness_validator"
 
-    def self.included(model_class)
-      query_method = Module.new do
-        define_method Mobility.query_method do
-          all
-        end
+    class QueryMethod < Module
+      def initialize(query_method)
+        module_eval <<-EOM, __FILE__, __LINE__ + 1
+          def #{query_method}
+            all
+          end
+        EOM
       end
-      model_class.extend query_method
+    end
+
+    def self.included(model_class)
+      model_class.extend QueryMethod.new(Mobility.query_method)
       unless model_class.const_defined?(:UniquenessValidator)
         model_class.const_set(:UniquenessValidator,
                               Class.new(::Mobility::ActiveRecord::UniquenessValidator))

f0c0baed8e1d4c5141b7e3c53e4c2b8917f83d5d
Chris Salzberg <chris@dejimata.com>
2018-03-24 15:38:01 +0900

    Column translations generator fix template formatting

--- lib/rails/generators/mobility/templates/column_translations.rb
+++ lib/rails/generators/mobility/templates/column_translations.rb
@@ -4,7 +4,7 @@ class <%= migration_class_name %> < <%= activerecord_migration_class %>
 <% I18n.available_locales.each do |locale| -%>
 <% column_name = Mobility.normalize_locale_accessor(attribute.name, locale) -%>
 <% if connection.column_exists?(table_name, column_name) -%>
-<% warn "#{column_name} already exists, skipping." %>
+<% warn "#{column_name} already exists, skipping." -%>
 <% else -%>
     add_column :<%= table_name %>, :<%= column_name %>, :<%= attribute.type %><%= attribute.inject_options %>
     <%- if attribute.has_index? -%>

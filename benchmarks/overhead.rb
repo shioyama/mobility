60f58305ebc01e53ed1785f53aa1e52151e87624
Chris Salzberg <chris@dejimata.com>
2018-05-08 21:31:07 +0900

    Add more detailed reports to benchmark
    [ci skip]

--- benchmarks/overhead.rb
+++ benchmarks/overhead.rb
@@ -12,7 +12,7 @@ I18n.available_locales = [ :en, :de, :ja ]
 I18n.default_locale = :en
 I18n.locale = :ja
 
-COLUMNS = %w(title body long_title seo_title)
+COLUMNS = %w(title content subtitle author)
 
 ActiveRecord::Schema.define(version: 0) do
   create_table :posts, force: true do |t|
@@ -30,14 +30,20 @@ end
 
 class Post < ActiveRecord::Base
   extend Mobility
-  translates(*COLUMNS, backend: :column, presence: false)
+  translates :title, backend: :column
+  translates :content, backend: :column, presence: false
+  translates :subtitle, backend: :column, presence: false, cache: false
+  translates :author, backend: :column, cache: false
 end
 
 post = Post.new(title_en: "hey", title_ja: "あああ")
 
 Benchmark.ips do |x|
   x.report("activerecord") { post.title_ja }
-  x.report("mobility without plugins") { post.title }
+  x.report("mobility with default plugins") { post.title }
+  x.report("mobility without presence plugin") { post.content }
+  x.report("mobility without presence and cache plugin") { post.subtitle }
+  x.report("mobility without cache plugin") { post.subtitle }
 
   x.compare!
 end

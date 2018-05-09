932d26f539e17b25da3c6b73f2030d2797876b59
Chris Salzberg <chris@dejimata.com>
2018-05-09 13:16:34 +0900

    Add spec for mobility[] method

--- spec/mobility/interface_spec.rb
+++ spec/mobility/interface_spec.rb
@@ -39,4 +39,30 @@ describe Mobility::Interface do
 
     end
   end
+
+  describe "#[]" do
+    before do
+      backend_class = Class.new do
+        include Mobility::Backend
+
+        def self.[](name, locale)
+          [name, locale]
+        end
+      end
+      stub_const "TestBackend", backend_class
+
+      attributes = Mobility::Attributes.new("foo", "bar", backend: TestBackend)
+      Class.new { include Mobility }.include(attributes)
+      subject << attributes
+    end
+
+    it "delegates to backend class" do
+      expect(subject[:foo, :en]).to eq([:foo, :en])
+    end
+
+    it "uses Mobility.locale as default locale" do
+      Mobility.with_locale(:fr) { expect(subject[:foo]).to eq([:foo, :fr]) }
+      Mobility.with_locale(:ja) { expect(subject[:foo]).to eq([:foo, :ja]) }
+    end
+  end
 end

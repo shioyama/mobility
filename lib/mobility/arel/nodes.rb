d30239f96874c6f0a3d17a85bf15278fda6d7ae6
Chris Salzberg <chris@dejimata.com>
2018-05-25 11:52:17 +0900

    Subclass Arel::Nodes::Grouping and Arel::Nodes::Equality
    This is important so that in the Query plugin when we invert a
    predicate, we correctly include nodes which inherit from
    Mobility::Arel::Grouping and Mobility::Arel::Equality.

--- lib/mobility/arel/nodes.rb
+++ lib/mobility/arel/nodes.rb
@@ -4,8 +4,8 @@ module Mobility
     module Nodes
       class Unary  < ::Arel::Nodes::Unary;  end
       class Binary < ::Arel::Nodes::Binary; end
-      class Grouping < Unary; end
-      class Equality < Binary; end
+      class Grouping < ::Arel::Nodes::Grouping; end
+      class Equality < ::Arel::Nodes::Equality; end
 
       ::Arel::Visitors::ToSql.class_eval do
         alias :visit_Mobility_Arel_Nodes_Equality :visit_Arel_Nodes_Equality

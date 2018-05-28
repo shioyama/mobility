dc8cf7255ec492bafd09dd244c1e01c892ef8512
Chris Salzberg <chris@dejimata.com>
2018-05-28 22:48:09 +0900

    Only run case-sensitive option spec when DB is postgres and AR >= 5.0

--- spec/support/shared_examples/querying_examples.rb
+++ spec/support/shared_examples/querying_examples.rb
@@ -410,13 +410,15 @@ shared_examples_for "AR Model with translated scope" do |model_class_name, a1=:t
           expect(query { __send__(a1).matches("%foo") }).to match_array([i[0], *i[5..6], barfoo])
         end
 
-        it "works with case_sensitive option" do
-          foobar = model_class.create(a1 => "foObar")
-          barfoo = model_class.create(a1 => "barfOo")
-          expect(query { __send__(a1).matches("foo%", nil, true) }).not_to include(foobar)
-          expect(query { __send__(a1).matches("foO%", nil, true) }).to include(foobar)
-          expect(query { __send__(a1).matches("%foo", nil, true) }).not_to include(barfoo)
-          expect(query { __send__(a1).matches("%fOo", nil, true) }).to include(barfoo)
+        if ENV['DB'] == 'postgres' && ::ActiveRecord::VERSION::STRING >= '5.0'
+          it "works with case_sensitive option" do
+            foobar = model_class.create(a1 => "foObar")
+            barfoo = model_class.create(a1 => "barfOo")
+            expect(query { __send__(a1).matches("foo%", nil, true) }).not_to include(foobar)
+            expect(query { __send__(a1).matches("foO%", nil, true) }).to include(foobar)
+            expect(query { __send__(a1).matches("%foo", nil, true) }).not_to include(barfoo)
+            expect(query { __send__(a1).matches("%fOo", nil, true) }).to include(barfoo)
+          end
         end
       end
 

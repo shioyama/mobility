710bba5e77f39b1588da6dff05101f49256d22b5
Chris Salzberg <chris@dejimata.com>
2018-05-29 12:34:46 +0900

    Remove default_scope when applying uniqueness validation

--- spec/support/shared_examples/validation_examples.rb
+++ spec/support/shared_examples/validation_examples.rb
@@ -22,6 +22,14 @@ shared_examples_for "AR Model validation" do |model_class_name, attribute1=:titl
         @instance2 = model_class.new(attribute1 => "foo")
         expect(@instance2).not_to be_valid
       end
+
+      context "with default_scope defined" do
+        it "removes default_scope" do
+          model_class.class_eval { default_scope { none } }
+          model_class.create(attribute1 => "foo")
+          expect(model_class.new(attribute1 => "foo")).not_to be_valid
+        end
+      end
     end
 
     context "with untranslated scope on translated attribute" do

ca5db8bbcfdbec7b5ebed16ba798b38b6cbfc8c5
Chris Salzberg <chris@dejimata.com>
2018-10-12 09:56:25 +0900

    Add Hash backend
    This is a "virtual" backend, in the sense that it is not backed by a db
    or coupled to any ORM. Very minimal but could be useful if you want to
    define e.g. fallbacks on attributes matching your persisted models.

---
+++ lib/mobility/backends/hash.rb
@@ -0,0 +1,37 @@
+module Mobility
+  module Backends
+=begin
+
+Backend which stores translations in an in-memory hash.
+
+=end
+    class Hash
+      include Backend
+
+      # @!group Backend Accessors
+      # @!macro backend_reader
+      # @return [Object]
+      def read(locale, _ = {})
+        translations[locale]
+      end
+
+      # @!macro backend_writer
+      # @return [Object]
+      def write(locale, value, _ = {})
+        translations[locale] = value
+      end
+      # @!endgroup
+
+      # @!macro backend_iterator
+      def each_locale
+        translations.each { |l, _| yield l }
+      end
+
+      private
+
+      def translations
+        @translations ||= {}
+      end
+    end
+  end
+end
---
+++ spec/mobility/backends/hash_spec.rb
@@ -0,0 +1,46 @@
+require "spec_helper"
+require "mobility/backends/hash"
+
+describe Mobility::Backends::Hash do
+  describe "#read/#write" do
+    it "returns value for locale key" do
+      backend = described_class.new
+      expect(backend.read(:ja)).to eq(nil)
+      expect(backend.read(:en)).to eq(nil)
+      backend.write(:ja, "アアア")
+      expect(backend.read(:ja)).to eq("アアア")
+      expect(backend.read(:en)).to eq(nil)
+      backend.write(:en, "foo")
+      expect(backend.read(:ja)).to eq("アアア")
+      expect(backend.read(:en)).to eq("foo")
+    end
+  end
+
+  describe "#each_locale" do
+    it "returns keys of hash to block" do
+      backend = described_class.new
+      backend.write(:ja, "アアア")
+      backend.write(:en, "aaa")
+      expect { |b| backend.each_locale(&b) }.to yield_successive_args(:ja, :en)
+    end
+  end
+
+  context "included in model" do
+    let(:model_class) do
+      klass = Class.new
+      klass.extend Mobility
+      klass.translates :name, backend: :hash
+      klass
+    end
+
+    it "defines reader and writer methods" do
+      instance = model_class.new
+      Mobility.with_locale(:en) { instance.name = "foo" }
+      Mobility.with_locale(:ja) { instance.name = "アアア" }
+      expect(instance.name(locale: :en)).to eq("foo")
+      expect(instance.name(locale: :ja)).to eq("アアア")
+
+      expect(instance.name_backend.locales).to match_array([:en, :ja])
+    end
+  end
+end

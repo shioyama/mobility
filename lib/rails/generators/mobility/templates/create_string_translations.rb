f5c14d3267b9c29435ebb2af11235d4bc3e42e83
Chris Salzberg <chris@dejimata.com>
2018-10-19 08:38:23 +0900

    Use references to define polymorphic associations in generator migrations

--- lib/rails/generators/mobility/templates/create_string_translations.rb
+++ lib/rails/generators/mobility/templates/create_string_translations.rb
@@ -2,12 +2,11 @@ class CreateStringTranslations < <%= activerecord_migration_class %>
 
   def change
     create_table :mobility_string_translations do |t|
-      t.string  :locale,            null: false
-      t.string  :key,               null: false
-      t.string  :value
-      t.integer :translatable_id,   null: false
-      t.string  :translatable_type, null: false
-      t.timestamps                  null: false
+      t.string :locale, null: false
+      t.string :key, null: false
+      t.string :value
+      t.references :translatable, polymorphic: true, index: false
+      t.timestamps null: false
     end
     add_index :mobility_string_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_string_translations_on_keys
     add_index :mobility_string_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_string_translations_on_translatable_attribute
--- lib/rails/generators/mobility/templates/create_text_translations.rb
+++ lib/rails/generators/mobility/templates/create_text_translations.rb
@@ -2,12 +2,11 @@ class CreateTextTranslations < <%= activerecord_migration_class %>
 
   def change
     create_table :mobility_text_translations do |t|
-      t.string  :locale,            null: false
-      t.string  :key,               null: false
-      t.text    :value
-      t.integer :translatable_id,   null: false
-      t.string  :translatable_type, null: false
-      t.timestamps                  null: false
+      t.string :locale, null: false
+      t.string :key,    null: false
+      t.text :value
+      t.references :translatable, polymorphic: true, index: false
+      t.timestamps null: false
     end
     add_index :mobility_text_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_text_translations_on_keys
     add_index :mobility_text_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_text_translations_on_translatable_attribute

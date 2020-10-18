bc6f49b30773facda20aa1a8ba88d052bb7dffc5
Chris Salzberg <chris@dejimata.com>
2020-10-18 12:30:29 +0900

    Update inline docs

--- lib/mobility/translations.rb
+++ lib/mobility/translations.rb
@@ -4,41 +4,51 @@ require "mobility/pluggable"
 module Mobility
 =begin
 
-Defines accessor methods to include on model class. Inspired by Traco's
-+Traco::Translations+ class.
+Module containing translation accessor methods and other methods for accessing
+translations.
 
-Normally this class will be created through class methods defined using
-{Mobility::Translates} accessor methods, and need not be created directly.
-However, the class is central to how Mobility hooks into models to add
-accessors and other methods, and should be useful as a reference when
-understanding and designing backends.
+Normally this class will be created by calling
+{Mobility::Translates#translates}, which is a class method on any model that
+extends {Mobility}, but it can also be used independent of that macro.
 
 ==Including Translations in a Class
 
 Since {Translations} is a subclass of +Module+, including an instance of it is
-like including a module. Creating an instance like this:
+like including a module. Options passed to the initializer are passed to
+plugins (if those plugins have been enabled).
 
-  Translations.new("title", backend: :my_backend, locale_accessors: [:en, :ja], cache: true, fallbacks: true)
+We can first enable plugins by subclassing `Mobility::Translations`, and
+calling +plugins+ to enable any plugins we want to use. We want reader and
+writer methods for accessing translations, so we enable those plugins:
 
-will generate an anonymous module that behaves approximately like this:
+  class Translations < Mobility::Translations
+    plugins do
+      reader
+      writer
+    end
+  end
+
+Both `reader` and `writer` depend on the `backend` plugin, so this is also enabled.
+
+Then create an instance like this:
+
+  Translations.new("title", backend: :my_backend)
+
+This will generate an anonymous module that behaves approximately like this:
 
   Module.new do
+    # From Mobility::Plugins::Backend module
+    #
     def mobility_backends
       # Returns a memoized hash with attribute name keys and backend instance
       # values.  When a key is fetched from the hash, the hash calls
       # +self.class.mobility_backend_class(name)+ (where +name+ is the
       # attribute name) to get the backend class, then instantiate it (passing
       # the model instance and attribute name to its initializer) and return it.
-      #
-      # The backend class returned from the class method
-      # +mobility_backend_class+ returns a subclass of
-      # +Mobility::Backends::MyBackend+ and includes into it:
-      #
-      # - Mobility::Plugins::Cache (from the +cache: true+ option)
-      # - instance of Mobility::Plugins::Fallbacks (from the +fallbacks: true+ option)
-      # - Mobility::Plugins::Presence (by default, disabled by +presence: false+)
     end
 
+    # From Mobility::Plugins::Reader module
+    #
     def title(locale: Mobility.locale)
       mobility_backends[:title].read(locale)
     end
@@ -47,45 +57,17 @@ will generate an anonymous module that behaves approximately like this:
       mobility_backends[:title].read(locale).present?
     end
 
+    # From Mobility::Plugins::Writer module
     def title=(value, locale: Mobility.locale)
       mobility_backends[:title].write(locale, value)
     end
-
-    # Start Locale Accessors
-    #
-    def title_en
-      title(locale: :en)
-    end
-
-    def title_en?
-      title?(locale: :en)
-    end
-
-    def title_en=(value)
-      public_send(:title=, value, locale: :en)
-    end
-
-    def title_ja
-      title(locale: :ja)
-    end
-
-    def title_ja?
-      title?(locale: :ja)
-    end
-
-    def title_ja=(value)
-      public_send(:title=, value, locale: :ja)
-    end
-    # End Locale Accessors
   end
 
-Including this module into a model class will thus add the backend method, the
-reader, writer and presence methods, and the locale accessor so the model
-class. (These methods are in fact added to the model in an +included+ hook.)
-
-Note that some simplifications have been made above for readability. (In
-reality, all getters and setters accept an options hash which is passed along
-to the backend instance.)
+Including this module into a model class will thus add the backends method and
+reader and writer methods for accessing translations. Other plugins (e.g.
+fallbacks, cache) modify the result returned by the backend, by hooking into
+the +included+ callback method on the module, see {Mobility::Plugin} for
+details.
 
 ==Setting up the Model Class
 

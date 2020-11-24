333ad5a1c4ba1033425d6d8de6b1cb91cd2836bd
Chris Salzberg <chris@dejimata.com>
2020-11-24 14:06:24 +0900

    Add docs

---
+++ website/docusaurus.config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  title: 'Mobility',
+  tagline: 'A Pluggable Translation Framework for Ruby',
+  url: 'https://chrissalzberg.com/mobility',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Mobility',
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/shioyama/mobility',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/mobility',
+            },
+            {
+              label: 'Gitter',
+              href: 'https://gitter.im/mobility-ruby/mobility',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/shioyama/mobility',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Chris Salzberg, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ website/src/pages/index.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+import Remarkable from 'remarkable';
+import RemarkableReactRenderer from 'remarkable-react';
+import hljs from 'highlight.js';
+import ruby from 'highlight.js/lib/languages/ruby';
+import 'highlight.js/styles/railscasts.css';
+hljs.registerLanguage('ruby', ruby);
+ 
+const md = new Remarkable();
+md.renderer = new RemarkableReactRenderer({
+  components: {
+    pre: ({ content, params: language }) => (
+      <pre className="code hljs" dangerouslySetInnerHTML={ { __html:
+        hljs.highlight(language, content).value,
+      } } />
+    ),
+  }
+});
+
+function Demo() {
+  return md.render(`
+## I18n for Model Data
+
+Choose a backend to store your translations:
+
+\`\`\`rb
+Mobility.configure do
+  plugins do
+    backend :key_value
+    active_record
+    reader
+    writer
+    # ...
+  end
+end
+\`\`\`
+
+Define a model and \`translate\` it:
+
+\`\`\`rb
+class Post < ApplicationRecord
+  extend Mobility
+  translates :title, :subtitle
+end
+\`\`\`
+
+Assign attributes:
+
+\`\`\`rb
+post = Post.new(
+  title: "Introduction to Mobility",
+  subtitle: "Translating Model Data is Easy"
+)
+post.title
+#=> "Mobility"
+post.subtitle
+#=> "Translating Model Data is Easy"
+post.save
+\`\`\`
+
+Translate them:
+
+\`\`\`rb
+Mobility.with_locale(:ja) do
+  post.title = "Mobilityの紹介"
+  post.subtitle = "モデルデータの翻訳を簡単に"
+end
+
+post.title
+#=> "Mobilityの紹介"
+
+post.subtitle
+#=> "モデルデータの翻訳を簡単に"
+\`\`\`
+
+
+And find them back:
+
+\`\`\`rb
+post = Post.i18n.find_by(title: "Introduction to Mobility")
+#=> #<Post id: 1, ...>
+
+post.title
+#=> "Introduction to Mobility"
+\`\`\`
+
+
+Or this:
+
+\`\`\`rb
+Post.i18n do
+  title.matches("%Mobility%")
+end
+
+# SELECT "posts".* FROM "posts"
+# LEFT OUTER JOIN "mobility_string_translations" "Post_title_en_string_translations"
+#   ON "Post_title_en_string_translations"."key" = 'title'
+#   AND "Post_title_en_string_translations"."locale" = 'en'
+#   AND "Post_title_en_string_translations"."translatable_type" = 'Post'
+#   AND "Post_title_en_string_translations"."translatable_id" = "posts"."id"
+# WHERE "Post_title_en_string_translations"."value" ILIKE '%Mobility%'
+\`\`\`
+
+And much, much more.
+
+`);
+};
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Hero() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return  (
+    <header className={clsx('hero hero--light', styles.heroBanner)}>
+      <div className="container margin-vert--lg">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <Link
+          className={clsx(
+            'button button--outline button--primary button--lg',
+            styles.getStarted,
+          )}
+          to={useBaseUrl('docs/getting-started')}>
+          Get Started
+        </Link>
+        <Link
+          className={clsx(
+            'button button--outline button--primary button--lg',
+            styles.getStarted,
+          )}
+          to={useBaseUrl('docs/upgrading-to-version-1')}>
+          Upgrading to 1.0
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Mobility · Pluggable Ruby Translation Framework`}
+      description="I18n for your Model Data">
+      <Hero />
+      <main>
+        <section className="mainContainer">
+          <div className="container">
+            <Demo />
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;

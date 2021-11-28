99579bb5f3bf9b3588fe5e7319bcb1b9e83131ec
Chris Salzberg <chris@dejimata.com>
2021-11-28 13:46:10 +0900

    Release 1.2.4

--- README.md
+++ README.md
@@ -55,7 +55,7 @@ Installation
 Add this line to your application's Gemfile:
 
 ```ruby
-gem 'mobility', '~> 1.2.3'
+gem 'mobility', '~> 1.2.4'
 ```
 
 ### ActiveRecord (Rails)
--- lib/mobility/version.rb
+++ lib/mobility/version.rb
@@ -8,7 +8,7 @@ module Mobility
   module VERSION
     MAJOR = 1
     MINOR = 2
-    TINY = 3
+    TINY = 4
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")

70e2492d37c1620ff7a94cd85b2c9666b9539ee3
Tim Krins <timkrins@gmail.com>
2023-06-02 10:58:47 +1000

    Add MySQL Visitors to json_ops

--- lib/mobility/plugins/arel/nodes/json_ops.rb
+++ lib/mobility/plugins/arel/nodes/json_ops.rb
@@ -46,9 +46,31 @@ module Mobility
             visit(Nodes::Grouping.new(::Arel::Nodes::InfixOperation.new(opr, o.left, o.right)), a)
           end
         end
+
+        module MySQL
+          def visit_Mobility_Plugins_Arel_Nodes_JsonDashArrow o, a
+            json_infix o, a, '->'
+          end
+
+          def visit_Mobility_Plugins_Arel_Nodes_JsonDashDoubleArrow o, a
+            # for MySQL we need to re-quote the JSON key name, otherwise it will break on keys like `pt-BR`
+            # https://dev.mysql.com/doc/refman/8.0/en/json-search-functions.html#operator_json-inline-path
+            unless o.right.value.to_s.start_with?('$.')
+              o.right = o.right.class.new(%Q($."#{o.right.value}"))
+            end
+            json_infix o, a, '->>'
+          end
+
+          private
+
+          def json_infix o, a, opr
+            visit(Nodes::Grouping.new(::Arel::Nodes::InfixOperation.new(opr, o.left, o.right)), a)
+          end
+        end
       end
 
       ::Arel::Visitors::PostgreSQL.include Visitors::PostgreSQL
+      ::Arel::Visitors::MySQL.include Visitors::MySQL
     end
   end
 end
